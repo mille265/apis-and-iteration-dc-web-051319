273c51943566d6850252c24a54fb685f44fff4a6
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-14 17:02:45 -0400

    we are done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,28 +4,39 @@ require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+  
+  
+  all_characters = RestClient.get('http://www.swapi.co/api/people/')
+  character_hash = JSON.parse(all_characters)
+    character_data = character_hash["results"].find { |data| data["name"].downcase == character_name }
+    if character_data != nil  
+      character_data["films"].collect do |url|
+          JSON.parse(RestClient.get(url))
+        end
+    else
+      get_character_movies_from_api(get_valid_char_name)
+    end
 end
 
-def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+def get_valid_char_name
+  puts "Please enter a VALID character name:"
+  character = gets.chomp.downcase
 end
+
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
 end
+
+
+def print_movies(films)
+  # some iteration magic and puts out the movies in a nice list
+  films.each_with_index do |film, i|
+    puts "#{i+1}. Episode #{film["episode_id"]}: #{film["title"]}"
+  end
+end
+
 
 ## BONUS
 
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,8 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome Star Wars fan!"
 end
 
 def get_character_from_user
-  puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  puts "Please enter a character name:"
+  input = gets.chomp.downcase
 end
